efd3ede567db7ef974c2e562b40c64908c42a34d
Robert F.P. Ludwick <rfpludwick@gmail.com>
2021-03-01 03:06:52 +0000

    Fixes up devcontainer for Jekyll

--- .devcontainer/provision.sh
+++ .devcontainer/provision.sh
@@ -8,6 +8,7 @@ cp .devcontainer/.bash-config/.bash_exports_local ~/.bash_exports_local
 apt update
 apt install -y ruby-full build-essential zlib1g-dev
 gem install jekyll bundler
+bundle install
 
 # Local provisioner?
 if [ -x .devcontainer/provision-local.sh ]; then
