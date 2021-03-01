2b4c95257a7c5c37b98f34d2e690434cee47cb17
Robert F.P. Ludwick <rfpludwick@gmail.com>
2021-03-01 01:06:07 +0000

    VSCode devcontainer added

---
+++ .devcontainer/provision.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+cp .devcontainer/.bash-config/.bash_exports_local ~/.bash_exports_local
+# shellcheck disable=SC1090
+. ~/.bash_exports_local
+apt update
+apt install -y ruby-full build-essential zlib1g-dev
+gem install jekyll bundler
+
+# Local provisioner?
+if [ -x .devcontainer/provision-local.sh ]; then
+	.devcontainer/provision-local.sh
+fi
